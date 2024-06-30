379b595b32b53784c6e5fde7be46e7a7f1f94eab
tretrak <3trak10@gmail.com>
2024-06-30 21:43:52 +0100

    add server workers55

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,2 +1,3 @@
-export const siteUrl = "https://t2v-express.onrender.com";
+type SiteUrl = "https://t2v-express.onrender.com" | "http://localhost:5000";
+export const siteUrl: SiteUrl = "https://t2v-express.onrender.com";
 //export const siteUrl = "http://localhost:5000";
